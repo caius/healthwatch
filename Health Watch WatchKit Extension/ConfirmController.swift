48e958b088eb790841ea46af7857999c65aa6db0
Caius Durling <dev@caius.name>
2016-11-23 03:43:07 +0000

    We have rudimentary UI flow working

--- Health Watch WatchKit Extension/ConfirmController.swift
+++ Health Watch WatchKit Extension/ConfirmController.swift
@@ -10,11 +10,24 @@ import WatchKit
 
 class ConfirmController: WKInterfaceController {
 
-  @IBOutlet var label: WKInterfaceLabel!
+  @IBOutlet var valueLabel: WKInterfaceLabel!
+
   var chosenValue: EntryPickerValue!
-  
+
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+
+    self.chosenValue = (context as! [String: Any])["value"] as! EntryPickerValue
+
+    guard chosenValue != nil else {
+      fatalError("No value passed to ConfirmController")
+    }
+
+    self.valueLabel.setText(chosenValue.title)
+  }
+
   @IBAction func confirmTapped() {
-    print("Action confirmed - We should save \(chosenValue)")
+    print("Action confirmed - We should save \(chosenValue.title)")
   }
 
   @IBAction func cancelTapped() {
