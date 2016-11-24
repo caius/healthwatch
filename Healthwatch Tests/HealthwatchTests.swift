8e397fda5c089706df661b21fe52da223c9a673f
Caius Durling <dev@caius.name>
2016-11-24 23:40:13 +0000

    Fix broken project

--- Healthwatch Tests/HealthwatchTests.swift
+++ Healthwatch Tests/HealthwatchTests.swift
@@ -9,8 +9,8 @@
 import XCTest
 @testable import Healthwatch
 
-class Healthwatch Tests: XCTestCase {
-    
+class HealthwatchTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,17 +20,16 @@ class Healthwatch Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
 }
